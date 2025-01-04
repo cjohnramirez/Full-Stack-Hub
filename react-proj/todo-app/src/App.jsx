71b7435ee865d609e088e41aefbdb01545290d12
cjohnramirez <jcramirez340@gmail.com>
2025-01-04 16:14:08 +0800

    Another New React Tutorial

---
+++ react-proj/todo-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from "./components/Header"
+import { Tabs } from "./components/Tabs"
+import { TodoList } from "./components/TodoList"
+import { TodoInput } from "./components/TodoInput"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Tabs />
+      <TodoList /> 
+      <TodoInput />
+    </>
+  )
+}
+
+export default App
