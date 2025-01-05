4b71f3b646588d5a02cdc1605f2f0e9918ed4f57
cjohnramirez <jcramirez340@gmail.com>
2025-01-05 15:02:20 +0800

    Implement todo app functionality with add, display, and filter features

--- react-proj/todo-app/src/App.jsx
+++ react-proj/todo-app/src/App.jsx
@@ -2,14 +2,32 @@ import { Header } from "./components/Header"
 import { Tabs } from "./components/Tabs"
 import { TodoList } from "./components/TodoList"
 import { TodoInput } from "./components/TodoInput"
+import { useState } from "react"
 
 function App() {
+  const [todos, setTodos] = useState([
+    { input: 'Hello! Add your first todo!', complete: true }
+  ])
+
+  function handleAddTodo(newTodo) {
+    const newTodoList = [...todos, {input: newTodo, complete: false}]
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo() {
+
+  }
+  
+  function handleDeleteTodo() {
+
+  }
+
   return (
     <>
-      <Header />
-      <Tabs />
-      <TodoList /> 
-      <TodoInput />
+      <Header todos={todos}/>
+      <Tabs todos={todos}/>
+      <TodoList todos={todos}/> 
+      <TodoInput handleAddTodo={handleAddTodo} />
     </>
   )
 }
--- react-proj/todo-app/src/components/Tabs.jsx
+++ react-proj/todo-app/src/components/Tabs.jsx
@@ -1,5 +1,23 @@
-export function Tabs() {
+export function Tabs(props) {
+  const { todos } = props
+
+  const tabs = ['All', 'Open', 'Completed'];
+
   return (
-    <nav></nav>
+    <nav className="tab-component">
+      {tabs.map((tab, index) => {
+        const numOfTasks = tab === 'All' ? 
+          todos.length : 
+          tab === 'Open' ?
+            todos.filter(val => !val.complete).length 
+            : todos.filter(val => val.complete).length
+
+        return (
+          <button key={index} className="tab-button">
+            <h4>{tab} <span> ({numOfTasks})</span></h4>
+          </button>
+        )
+      })}
+    </nav>
   )
 }
